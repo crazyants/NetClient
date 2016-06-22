8620341d9e9a9c3e0c125c9074d945834f5beadf
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-22 12:04:57 -0600

    Add error support on the client interface.

--- Source/NetClient/INetClient.cs
+++ Source/NetClient/INetClient.cs
@@ -1,9 +1,16 @@
-﻿namespace NetClient
+﻿using System;
+
+namespace NetClient
 {
     /// <summary>
     ///     Provides a interface for sending requests and receiving responses over network boundaries.
     /// </summary>
     public interface INetClient
     {
+        /// <summary>
+        ///     Gets or sets the error action.
+        /// </summary>
+        /// <value>The error action.</value>
+        Action<Exception> OnError { get; set; }
     }
 }
