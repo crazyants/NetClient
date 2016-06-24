e5e61c5aae9499692bfe71ac3301556aa840fd73
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-23 23:21:05 -0600

    Update element to support criteria.

--- Source/NetClient/IElement.cs
+++ Source/NetClient/IElement.cs
@@ -6,8 +6,24 @@ namespace NetClient
     /// <summary>
     ///     The NetClient element.
     /// </summary>
-    /// <typeparam name="T">Type of element.</typeparam>
-    public interface IElement<out T> : IQueryable<T>
+    /// <typeparam name="T">The type of the element.</typeparam>
+    /// <typeparam name="TCriteria">The type of the criteria.</typeparam>
+    public interface IElement<out T, out TCriteria> : IElement, IQueryable<TCriteria>
+    {
+    }
+
+    /// <summary>
+    ///     The NetClient element.
+    /// </summary>
+    /// <typeparam name="T">The type of the element.</typeparam>
+    public interface IElement<out T> : IElement, IQueryable<T>
+    {
+    }
+
+    /// <summary>
+    ///     The NetClient element.
+    /// </summary>
+    public interface IElement
     {
         /// <summary>
         ///     Gets the client.
